fc8192fd0921ebb951fb825d128ea06cbdef4df8
Siddhant <115331356+grimsteel@users.noreply.github.com>
2023-12-10 16:16:42 -0600

    Sessions and requests

--- src/injected-script.js
+++ src/injected-script.js
@@ -71,5 +71,77 @@ if (location.pathname.includes("manage/profile")) {
       });
     }
   })
+} else if (location.pathname.includes("requests/requestsessions")) {
+  const sessionContainer = document.getElementById("sessionContainer");
+  addEventListener("load", () => {
+    const oldCreateSessionList = window.createSessionList;
+    const oldClickSession = window.clickSession;
+    const oldCreateRequestList = window.createRequestList;
+
+    // Fix borders and spacing on sessions when they are created
+    window.createSessionList = function () {
+      oldCreateSessionList();
+
+      document.querySelectorAll(".session").forEach(el => {
+        el.classList.remove("bg-light", "border", "border-secondary", "rounded");
+        el.classList.add("card", "bg-secondary-bg");
+        for (const child of el.children) {
+          child.classList.remove("row");
+          child.style.removeProperty("margin");
+        }
+
+        // Add the additional percentages to the badge
+        const sessionData = window.sessionDict[el.id];
+        const sessionSizeData = window.sessionsizes[el.id];
+        const numSeats = +sessionData.openseats;
+        const numApproved = +sessionSizeData.hinum;
+        const numRequested = +sessionSizeData.reqnum;
+        const percentApproved = Math.round(numApproved / numSeats * 100);
+        const percentRequested = Math.round(numRequested / numSeats * 100);
+        el.querySelector(".badge").innerText += ` (${percentApproved}% / ${percentRequested}%)`;
+        el.querySelector(".badge").title = `${numApproved} approved / ${numRequested} requested / ${numSeats} seats`;
+      });
+    }
+
+    // Apply our changes to the expanded sessions
+    window.clickSession = function (session) {
+      oldClickSession(session);
+      const sessionEl = session.get(0);
+
+      sessionEl.querySelectorAll(".row").forEach(el => {
+        el.classList.remove("row");
+        el.style.removeProperty("margin");
+      });
+
+      sessionEl.querySelectorAll(".input-group-prepend").forEach(el => {
+        el.replaceWith(el.firstElementChild);
+      });
+
+      sessionEl.querySelector("#helpIcon")?.classList.remove("bg-white");
+      sessionEl.querySelector("#helpNeeded")?.classList.remove("border-secondary");
+      sessionEl.querySelector("#helpNeeded")?.classList.add("align-items-center");
+    }
+
+    // Fix borders and spacing on requests when they are created
+    window.createRequestList = function () {
+      oldCreateRequestList();
+
+      document.querySelectorAll(".request-date").forEach(el => {
+        el.classList.remove("bg-white", "border", "border-dark", "rounded");
+        el.classList.add("card", "bg-secondary-bg");
+        // Date header
+        el.firstElementChild.classList.remove("bg-secondary", "text-white", "rounded", "p-2");
+        el.firstElementChild.classList.add("alert", "alert-secondary", "py-2");
+
+        el.querySelectorAll(".input-group-prepend").forEach(child => {
+          child.replaceWith(child.firstElementChild);
+        });
+
+        el.lastElementChild.classList.remove("p-1");
+        el.querySelector(".input-group").classList.remove("ml-1", "border", "border-secondary");
+        el.querySelector(".form-control").classList.add("align-items-center", "d-flex", "flex-wrap");
+      });
+    }
+  });
 }
 
